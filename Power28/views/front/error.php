34c6b67d692f4229d36a1099c2cd4bed01261127
LaureCompoint <compointlaure@gmail.com>
2018-06-29 13:49:08 +0200

    udapte 2

---
+++ Power28/views/front/error.php
@@ -0,0 +1,6 @@
+<?php require ('Partials/nav.php');?>
+<div class="row">
+    <img src="assets/img/erreur.png" class="col-7 d-block mt-5" alt="logoadmin"/>
+    <img src="assets/img/adminlogo.png" class="rounded mx-auto d-block mt-5" style="height: 500px" alt="logoadmin"/>
+
+</div>
